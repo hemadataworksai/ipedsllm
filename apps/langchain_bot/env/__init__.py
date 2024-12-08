52497c7b20aa46c3a0e36001fe1108c8004942db
lnachman <lnachman@akamai.com>
2024-12-08 08:32:26 +0200

    fix: make its working docker

--- apps/langchain_bot/env/__init__.py
+++ apps/langchain_bot/env/__init__.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
-LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 db_url = os.getenv("DB_URL")
 redis_url = os.getenv("REDIS_URL")
 redis_token = os.getenv("REDIS_TOKEN")
