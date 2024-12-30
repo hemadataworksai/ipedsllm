759f0e15e9e62bc9d72598ee7a719b4049c94e08
hemadataworksai <155760218+hemadataworksai@users.noreply.github.com>
2024-12-30 13:56:00 -0500

    Update __init__.py
    Adding TODO
    
    Signed-off-by: hemadataworksai <155760218+hemadataworksai@users.noreply.github.com>

--- apps/langchain_bot/dependencies/__init__.py
+++ apps/langchain_bot/dependencies/__init__.py
@@ -5,7 +5,7 @@ from apps.langchain_bot.env import db_url
 from apps.langchain_bot.phases.context.vectors_store_sentence_transformer import DocumentRetriever
 from apps.langchain_bot.utils.db_information.database_information_retrieval import DatabaseInformationRetrieval
 from apps.langchain_bot.utils.llm_provider.llm_provider import LLMProvider
-
+#TODO: Add comments- such as these are Helper functions to do x, y, x
 database_information_retrieval = DatabaseInformationRetrieval(db_url=db_url)
 document_retriever = DocumentRetriever()
 llm = LLMProvider(provider=os.getenv("LLM_PROVIDER",None))
